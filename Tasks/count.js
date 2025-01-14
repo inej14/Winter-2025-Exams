82fd61e23304bac66af6b04da2c7d64e5e4be5b0
Your Name <yevheniiaharmash10@gmail.com>
2025-01-14 13:10:09 +0200

    Removed extra brackets and declared variable

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,13 @@
 // Refactor following solution
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+const count = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
