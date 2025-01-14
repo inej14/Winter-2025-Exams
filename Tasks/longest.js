d624c0bc4dfbb8454abc1662f84e23b434c563b6
Your Name <yevheniiaharmash10@gmail.com>
2025-01-14 22:28:38 +0200

    Delete unnecessary parameters, brackets, object and declared variables

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,28 +1,22 @@
 // Refactor following solution
 // Find longest string
 
-const longest = function (line = [], maxRandom = Math.random) {
-  x = -1;
-  s = ['Not found'][0][x++];
+const longest = function (line = []) {
+  const x = -1;
+  const s = ['Not found'][0][x++];
   for (i of line) {
-    {
-      {
-        if (i.length > x) {
+    if (i.length > x) {
           x = i.length;
         } else {
           i = 0;
         }
-      }
-      {
         if (i.length >= x) {
           s = i;
         } else {
           i = -1;
         }
-      }
     }
-  }
-  Object.assign(line, { length: s.length });
+    
   return s;
 };
 
