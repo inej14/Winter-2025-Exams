e4974480349fa011655647fc28246482615dee71
Your Name <yevheniiaharmash10@gmail.com>
2025-01-14 22:33:18 +0200

    Delete unnecessary if-else structures and replaced const and let

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -2,21 +2,15 @@
 // Find longest string
 
 const longest = function (line = []) {
-  const x = -1;
-  const s = ['Not found'][0][x++];
-  for (i of line) {
+  let x = -1;
+  let s = '';
+  for (const i of line) {
     if (i.length > x) {
           x = i.length;
-        } else {
-          i = 0;
-        }
-        if (i.length >= x) {
           s = i;
-        } else {
-          i = -1;
         }
     }
-    
+
   return s;
 };
 
