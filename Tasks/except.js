66dbaa95990ffd5881ac4f0b2363ce9af7d433cd
Your Name <yevheniiaharmash10@gmail.com>
2025-01-14 21:10:21 +0200

    Fixed variables and deleted unnecessary blocks of code

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,20 +1,15 @@
 // Refactor following solution
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
+const except = (incomingvaluesarray, ...no) => {
   intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
-    [].sort(() => 2000);
     if (no.includes(Z)) {
       delete incomingvaluesarray[Z];
       return;
-    } else {
-      return;
-      delete incomingvaluesarray[Z];
-    }
-  }, 2000);
-  ({ key: 'value' });
+     }
+    });
   return incomingvaluesarray;
-};
+}
 
-module.exports = EXCEPT;
+module.exports = except;
